2583a2854c8bf83a5a95f9ef71ae2898b500e81f
Veverre <hrv.leroy@gmail.com>
2025-09-25 12:20:43 +0200

    chore(backend): force api running on 4000

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,18 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { BoardsModule } from './boards/boards.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule, BoardsModule, TasksModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    BoardsModule,
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [
     {
--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 4000;
+  const host = process.env.HOST || 'localhost';
+
+  await app.listen(port, host);
 }
 bootstrap();
