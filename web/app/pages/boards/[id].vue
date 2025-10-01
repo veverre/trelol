af587453661e0edba0cd3096501789211a56eb8d
Veverre <hrv.leroy@gmail.com>
2025-10-01 16:25:39 +0200

    feat(frontend): display board's columns

--- web/app/pages/boards/[id].vue
+++ web/app/pages/boards/[id].vue
@@ -1,4 +1,6 @@
 <script setup>
+import { TaskStatus } from 'api';
+const statuses = Object.values(TaskStatus);
 const route = useRoute();
 
 const boardId = computed(() => route.params.id);
@@ -11,5 +13,15 @@ const board = computed(() => boardsStore.board);
     <div>
         <h1>{{ board.title }}</h1>
         <p>{{ board.description }}</p>
+        <div class="grid grid-cols-3 gap-4">
+            <div v-for="statut in statuses" :key="statut" class="bg-gray-100 p-4 rounded">
+                <h2 class="text-lg font-bold mb-2">{{ statut }}</h2>
+                <!-- <ul>
+                    <li v-for="task in column.tasks" :key="task.id" class="mb-1 p-2 bg-white rounded shadow">
+                        {{ task.title }}
+                    </li>
+                </ul> -->
+            </div>
+        </div>
     </div>
 </template>
