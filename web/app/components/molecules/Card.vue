e212490ad2aa822ccb6ff62a05c4fdd621d778c9
Veverre <hrv.leroy@gmail.com>
2025-10-07 12:23:46 +0200

    feat(frontend): create Card molecule

---
+++ web/app/components/molecules/Card.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full max-w-md p-4 bg-white rounded shadow">
+        <h1 class="text-xl font-semibold text-highlighted">
+            <slot name="heading"></slot>
+        </h1>
+        <h2 class="mt-1 text-base text-pretty text-muted">
+            <slot name="subHeading"></slot>
+        </h2>
+        <slot></slot>
+    </div>
+</template>
